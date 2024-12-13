85667f88ea17818dce94b7557fdbe02dde4162e6
Sebastiaan <sebastiaan@blauwe-lucht.nl>
2024-12-13 12:38:45 +0000

    Added: SolveLinearEquations2x2 to helpers.

--- helpers/Solver/Algorithms.cs
+++ helpers/Solver/Algorithms.cs
@@ -18,4 +18,26 @@ public static class Algorithms
         return b == 0 ? a : GCD(b, a % b);
     }
 
+    /// <summary>
+    /// Solve linear equations of the form:
+    /// a1 * x + b1 * y = c1
+    /// a2 * x + b2 * y = c2
+    /// </summary>
+    public static (bool hasSolution, long x, long y) SolveLinearEquations2x2(long a1, long b1, long c1, long a2, long b2, long c2)
+    {
+        // From https://byjus.com/maths/cramers-rule/
+        long D = a1 * b2 - a2 * b1;
+        long Dx = c1 * b2 - c2 * b1;
+        long Dy = a1 * c2 - a2 * c1;
+
+        if (Dx % D != 0 || Dy % D != 0)
+        {
+            return (false, 0, 0);
+        }
+
+        long x = Dx / D;
+        long y = Dy / D;
+
+        return (true, x, y);
+    }
 }
